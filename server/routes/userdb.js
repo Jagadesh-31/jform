968475ea4be7424808d036485b70e3df361db87a
jagadesh-31 <jjagadesh980@gmail.com>
2025-10-04 23:34:08 +0530

    error fixed for Docker

--- server/routes/userdb.js
+++ server/routes/userdb.js
@@ -1,12 +1,10 @@
 let express = require('express');
 const multer = require('multer');
 
-let {registerUser,loginUser,autoLoginUser,findUser,uploadImage} = require('../controllers/auth.js');
+let {registerUser,loginUser,autoLoginUser,findUser} = require('../controllers/auth.js');
 
 let dbRoutes = express.Router();
 
-
-const upload = multer({dest: "uploads/"})
 
 
 dbRoutes.post('/register',registerUser);
@@ -14,6 +12,5 @@ dbRoutes.post('/login',loginUser);
 dbRoutes.get('/verify',autoLoginUser);
 dbRoutes.get('/find',findUser);
 
-dbRoutes.post('/uploadImage', upload.single('image'),uploadImage)
 
 module.exports=dbRoutes;
